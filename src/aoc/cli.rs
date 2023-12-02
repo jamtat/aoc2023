3083e53e9cdaa198a51384eb77e76d60e04101f9
Ben Tattersley <ubershinysheep@gmail.com>
2023-12-02 15:10:50 +0000

    Default value for input file

--- src/aoc/cli.rs
+++ src/aoc/cli.rs
@@ -1,4 +1,5 @@
 use std::{
+    env,
     fs::{read_to_string, File},
     io::{BufRead, BufReader},
 };
@@ -7,18 +8,33 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct Cli {
-    pub input: std::path::PathBuf,
+    pub input: Option<std::path::PathBuf>,
 }
 
 impl Cli {
     pub fn line_reader(&self) -> impl Iterator<Item = String> + '_ {
-        let f = File::open(&self.input).unwrap();
+        let f = File::open(&self.input_file()).unwrap();
 
         BufReader::new(f).lines().map(|l| l.unwrap())
     }
 
     pub fn input_string(&self) -> String {
-        read_to_string(&self.input).unwrap()
+        read_to_string(&self.input_file()).unwrap()
+    }
+
+    pub fn input_file(&self) -> std::path::PathBuf {
+        if let Some(f) = &self.input {
+            f.clone()
+        } else {
+            let day_name = env::args()
+                .last()
+                .unwrap()
+                .rsplit_once("/")
+                .unwrap()
+                .1
+                .to_owned();
+            format!("input/{}.txt", day_name).into()
+        }
     }
 }
 
